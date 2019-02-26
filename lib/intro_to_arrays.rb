08adb9b85e2fc7f79447771821cfbf40dedcc48e
Umar Khan <umarkhandev@gmail.com>
2019-02-26 17:26:59 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  array = ["one", "two"]
+end
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  arr
+end
